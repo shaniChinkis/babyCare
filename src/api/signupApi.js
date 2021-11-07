b66945b79b947e4d1c0c8fc821722239f60ce549
shani ch <shanic0527@gmail.com>
2021-11-07 19:24:18 +0200

    changes

--- src/api/signupApi.js
+++ src/api/signupApi.js
@@ -31,9 +31,6 @@ export const signupGApi = (email, password) => {
     return fetch('http://localhost:3001/signUpG',
     {
         method:'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body:JSON.stringify({email:email,
               password:password})
         })
